cf769c310309b327c66d18be107b2ffebe53e1e4
raliceingabire <ingabireralice@yahoo.com>
2023-10-18 01:38:00 -0700

    sum

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include "4-sum.h"
+#include "4-sum.h"
 
-#define SUM(x, y) ((x) + (y))
-int main(void) {
-	int num1 = 10;
-	int num2 = 20;
-	int result = SUM(num1, num2);
-	printf("The sum of %d and %d is %d\n", num1, num2, result);
-	return 0;
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int s;
 
-
+    s = SUM(98, 1024);
+    printf("%d\n", s);
+    return (0);
 }
