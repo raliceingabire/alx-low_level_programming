757e7899a37c2e6889d32acb722dfca5d65eb4ad
raliceingabire <ingabireralice@yahoo.com>
2023-10-17 23:58:24 -0700

    fil name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main(void) {
+
+/**
+ * main - entry point
+ * Description - this is a program that prints the name of the file
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
 printf("compiled from file: %s\n", __FILE__);
 return 0;
 }
